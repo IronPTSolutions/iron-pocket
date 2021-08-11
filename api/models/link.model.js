389efe4ca062acac75905f15efaecf93175d6635
BlancaML <blancamorenodeluna@gmail.com>
2021-08-11 21:02:58 +0200

    feat: api

--- api/models/link.model.js
+++ api/models/link.model.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const URL_PATTERN = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
+
 /**
   | Attribute   | Type     | Validation               |
   |-------------|----------|--------------------------|
@@ -13,11 +15,44 @@ const Schema = mongoose.Schema;
 
 const linkSchema = new Schema(
   {
-    // TODO: model attributes validations
+    url: {
+      type: String,
+      match: [URL_PATTERN, 'URL is not valid'],
+      required: [ true, 'URL is required'],
+      unique: true,
+      
+    },
+
+    title: {
+      type: String
+    },
+
+    description: {
+      type: String
+    },
+
+    image: {
+      type: String
+    },
+    keywords: {
+      type: [String],
+      default: [],
+
+    }
   },
+
   {
     timestamps: true,
     // TODO: toJSON transformation
+    toJSON: {
+      virtuals: true,
+      transform: function (doc, ret) {
+        ret.id = ret._id;
+        delete ret.__v;
+        delete ret._id;
+        return ret;
+      }
+    }
   }
 )
 
