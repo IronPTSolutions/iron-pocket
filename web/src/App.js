be43b04149ee8d2751bcdd5ec26e0d74ea62da65
Manuela Ghiulai <gmanuelaromina@gmail.com>
2021-08-17 18:25:38 +0200

    feat: footer component

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,12 @@
 import { Route, Switch } from "react-router-dom";
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="row">
-    <Header />   
+    <Header /> 
+    <Footer />
     </div>
   );
 }
---
+++ web/src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer className="bg-light text-center text-lg-start">
+            <div className="text-center p-3" style={{backgroundColor: `rgba(${0}, ${0}, ${0}, ${0.2})`}}>
+                by: Manuela Ghiulai
+            </div>
+        </footer>
+    )
+}
+export default Footer;
